bf154d77470aa1a0509d431f6862185095bd8d1e
naeemlyon <Muhammad.Naeem@univ-lyon2.fr>
2016-12-08 01:25:45 +0100

    Web Content Miner

---
+++ WebContentMiner/frmWebContentMiner.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace WebContentMiner
+{
+    partial class frm_WebContentMiner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_WebContentMiner));
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.tbx_Root_Path = new System.Windows.Forms.TextBox();
+            this.tbx_Display = new System.Windows.Forms.TextBox();
+            this.lvw_Authors = new System.Windows.Forms.ListView();
+            this.columnHeader28 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader27 = new System.Windows.Forms.ColumnHeader();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Refresh = new System.Windows.Forms.Button();
+            this.cbo_To = new System.Windows.Forms.ComboBox();
+            this.cbo_From = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Start.Location = new System.Drawing.Point(512, 351);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(67, 40);
+            this.btn_Start.TabIndex = 0;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // tbx_Root_Path
+            // 
+            this.tbx_Root_Path.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_Root_Path.Location = new System.Drawing.Point(143, 23);
+            this.tbx_Root_Path.Multiline = true;
+            this.tbx_Root_Path.Name = "tbx_Root_Path";
+            this.tbx_Root_Path.Size = new System.Drawing.Size(419, 33);
+            this.tbx_Root_Path.TabIndex = 1;
+            this.tbx_Root_Path.Text = "E:\\\\WebContentMinerDownloads";
+            // 
+            // tbx_Display
+            // 
+            this.tbx_Display.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_Display.ForeColor = System.Drawing.Color.MediumBlue;
+            this.tbx_Display.Location = new System.Drawing.Point(9, 352);
+            this.tbx_Display.Multiline = true;
+            this.tbx_Display.Name = "tbx_Display";
+            this.tbx_Display.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbx_Display.Size = new System.Drawing.Size(341, 36);
+            this.tbx_Display.TabIndex = 95;
+            // 
+            // lvw_Authors
+            // 
+            this.lvw_Authors.BackColor = System.Drawing.Color.PowderBlue;
+            this.lvw_Authors.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader28,
+            this.columnHeader27});
+            this.lvw_Authors.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvw_Authors.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lvw_Authors.FullRowSelect = true;
+            this.lvw_Authors.GridLines = true;
+            this.lvw_Authors.HideSelection = false;
+            this.lvw_Authors.Location = new System.Drawing.Point(9, 62);
+            this.lvw_Authors.MultiSelect = false;
+            this.lvw_Authors.Name = "lvw_Authors";
+            this.lvw_Authors.Size = new System.Drawing.Size(567, 284);
+            this.lvw_Authors.TabIndex = 96;
+            this.lvw_Authors.UseCompatibleStateImageBehavior = false;
+            this.lvw_Authors.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader28
+            // 
+            this.columnHeader28.Text = "Authors";
+            this.columnHeader28.Width = 200;
+            // 
+            // columnHeader27
+            // 
+            this.columnHeader27.Text = "Home Page";
+            this.columnHeader27.Width = 350;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 97;
+            this.label1.Text = "Download Directory Path";
+            // 
+            // btn_Refresh
+            // 
+            this.btn_Refresh.Location = new System.Drawing.Point(404, 381);
+            this.btn_Refresh.Name = "btn_Refresh";
+            this.btn_Refresh.Size = new System.Drawing.Size(45, 16);
+            this.btn_Refresh.TabIndex = 111;
+            this.btn_Refresh.Text = "\'\'\'\'\'\'\'\'\'\'";
+            this.btn_Refresh.UseVisualStyleBackColor = true;
+            this.btn_Refresh.Click += new System.EventHandler(this.btn_Refresh_Click);
+            // 
+            // cbo_To
+            // 
+            this.cbo_To.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbo_To.FormattingEnabled = true;
+            this.cbo_To.Location = new System.Drawing.Point(435, 351);
+            this.cbo_To.Name = "cbo_To";
+            this.cbo_To.Size = new System.Drawing.Size(60, 21);
+            this.cbo_To.TabIndex = 110;
+            // 
+            // cbo_From
+            // 
+            this.cbo_From.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbo_From.FormattingEnabled = true;
+            this.cbo_From.Location = new System.Drawing.Point(357, 352);
+            this.cbo_From.Name = "cbo_From";
+            this.cbo_From.Size = new System.Drawing.Size(59, 21);
+            this.cbo_From.TabIndex = 109;
+            // 
+            // frm_WebContentMiner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 401);
+            this.Controls.Add(this.btn_Refresh);
+            this.Controls.Add(this.cbo_To);
+            this.Controls.Add(this.cbo_From);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lvw_Authors);
+            this.Controls.Add(this.tbx_Display);
+            this.Controls.Add(this.tbx_Root_Path);
+            this.Controls.Add(this.btn_Start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frm_WebContentMiner";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Web Content Miner (naeem@email.com)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.TextBox tbx_Root_Path;
+        private System.Windows.Forms.TextBox tbx_Display;
+        private System.Windows.Forms.ListView lvw_Authors;
+        private System.Windows.Forms.ColumnHeader columnHeader28;
+        private System.Windows.Forms.ColumnHeader columnHeader27;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Refresh;
+        private System.Windows.Forms.ComboBox cbo_To;
+        private System.Windows.Forms.ComboBox cbo_From;
+    }
+}
+
---
+++ WebContentMiner/frmWebContentMiner.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using System.Net;
+using System.Text.RegularExpressions;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+
+namespace WebContentMiner
+{
+    public partial class frm_WebContentMiner : Form
+    {
+        const String App_Title = "Web Content Miner (naeem@email.com)";
+        const String LastValueFile =  "C:\\Last_Value.txt";
+        
+        public frm_WebContentMiner()
+        {
+            InitializeComponent();
+            tbx_Display.Text = Populate_All_Home_Pages();
+            int i = 0;
+            for (i = 0; i < lvw_Authors.Items.Count; i++)
+            {
+                cbo_From.Items.Add(i.ToString());
+                cbo_To.Items.Add(i.ToString());
+            }
+            What_was_Latest_Value();
+            this.Text = App_Title;
+        }
+
+
+        private void What_was_Latest_Value()
+        {   
+            int Last_Value = 0;
+            try
+            {
+                StreamReader SR = File.OpenText(LastValueFile);
+                Last_Value = int.Parse(SR.ReadToEnd());
+                SR.Close();
+                SR.Dispose();
+            }
+            catch
+            { }
+
+            if (lvw_Authors.Items.Count <= Last_Value)
+                Last_Value = 0;
+            cbo_From.SelectedIndex = Last_Value;
+            if ((Last_Value + 9) >= lvw_Authors.Items.Count)
+                cbo_To.SelectedIndex = lvw_Authors.Items.Count - 1;
+            else
+                cbo_To.SelectedIndex = Last_Value + 9;
+        }
+
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            String[] Arr = Application.StartupPath.Split("\\".ToCharArray());
+            this.Text = Arr[Arr.Length - 1].ToString() + " (" + cbo_From.Text + " - " + cbo_To.Text + ") " + App_Title;
+            File.Delete(LastValueFile);
+            File.AppendAllText(LastValueFile, (int.Parse(cbo_To.Text) + 1).ToString());
+            //------------------------            
+            
+            String Link = String.Empty;
+            String AuthorName = String.Empty; 
+            String Root_Path = tbx_Root_Path.Text;
+            int i = 0;            
+            String Pth = String.Empty;
+            int From = int.Parse(cbo_From.Text);
+            int To = int.Parse(cbo_To.Text);
+
+            for (i = From; i <= To; i++)
+            {
+                AuthorName = lvw_Authors.Items[i].Text;
+                Link = lvw_Authors.Items[i].SubItems[1].Text;             
+                Pth = Root_Path + "\\" + AuthorName;
+                Directory.CreateDirectory(Pth);
+                tbx_Display.Text = i.ToString() + ": " + AuthorName;
+                Start_Fetcher(Link, Pth);
+                tbx_Display.Refresh();
+
+            }
+            tbx_Display.Text = "Processing completed...";
+        }
+
+        #region 
+        private void Start_Fetcher(String URL, String Path)
+        {
+            // collect all of the link found on this Author's HomePage
+            Hashtable HomePage_All_URLS = Mine_Out_HyperLink(URL);
+            String WebPageData = String.Empty;
+            int i = 1;
+
+            // Fech out all of the web page data..                                    
+            IDictionaryEnumerator link = HomePage_All_URLS.GetEnumerator();
+            while (link.MoveNext())
+            {
+                try
+                {   
+                    WebPageData = Fetch_WebPage_Data(link.Key.ToString()) + Environment.NewLine;
+                    File.AppendAllText(Path + "\\" + i.ToString() + ".htm", WebPageData);
+                    i++;
+                }
+                catch
+                {
+                    // do nothing... or try to debug out the reason of failure...
+                }
+            }                
+                
+        }
+        #endregion
+
+
+        #region Fetch WebPage Data
+        private String Fetch_WebPage_Data(String URL)
+        {
+            String Result = String.Empty;
+
+            if (URL.Trim().Length < 2) return Result;
+
+            if (Uri.IsWellFormedUriString(URL, UriKind.Absolute) == false) return Result;
+
+            // cv found
+            if (URL.ToLower().Contains(".pdf") == true)
+                return Read_PDF(URL);
+
+            try
+            {
+                WebResponse objResponse;
+                WebRequest objRequest = System.Net.HttpWebRequest.Create(URL);
+                objResponse = objRequest.GetResponse();
+
+                using (StreamReader sr = new StreamReader(objResponse.GetResponseStream()))
+                {
+                    Result = sr.ReadToEnd();
+                    // Close and clean up the StreamReader
+                    sr.Close();
+                }
+
+                // meta http-equiv=\"Refresh\" page is being refreshed. grab the new link from it..    
+                Result = Result.ToLower();
+                if ((Result.Contains("meta") == true) && (Result.Contains("http-equiv") == true) && (Result.Contains("refresh") == true))
+                {
+                    Result = Result.Substring(Result.LastIndexOf("url=") + 4);
+                    Result = Result.Replace("\">", "");
+                    Result += Fetch_WebPage_Data(Result.Trim());
+                }
+
+            }
+            catch
+            {
+                return Result;
+            }
+
+
+
+
+            return Result;
+        }
+        #endregion
+        
+        #region Mine_Out_HyperLink(String link)
+        private Hashtable Mine_Out_HyperLink(String link)
+        {
+            Hashtable Ret = new Hashtable();
+            if (Uri.IsWellFormedUriString(link, UriKind.Absolute) == false) return Ret;
+            Ret.Add(link, "Author Personal Web Page"); // first entry is Author's main personal web page..
+            String LinkName = String.Empty;
+            String HREF = String.Empty;
+
+            WebBrowser web = new WebBrowser();
+            web.NewWindow += new CancelEventHandler(web_NewWindow);
+            web.Navigate(link);
+
+            //wait for the browser object to load the page
+            while (web.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+
+            for (int i = 0; i < web.Document.Links.Count; i++)
+            {
+                if ((web.Document.Links[i].GetAttribute("href") != null))
+                {
+                    HREF = web.Document.Links[i].GetAttribute("href").ToString();
+                    if (Ret.Contains(HREF) == false)
+                    {
+                        LinkName = web.Document.Links[i].InnerText;
+                        if ((String.IsNullOrEmpty(LinkName) == true) || (String.IsNullOrEmpty(HREF) == true))
+                        { } // do nothing..
+
+                        else if ((HREF.Contains(".ps") == true) ||
+                            (HREF.Contains(".doc") == true) ||
+                            (HREF.Contains(".docx") == true) ||
+                            (HREF.Contains(".xls") == true) ||
+                            (HREF.Contains(".xlsx") == true)
+                            )
+                        { } // Do Nothing. We did not process these MIME
+
+                        // whether it contains cv link then must include it.
+                        else if ((HREF.Contains(".pdf") == true) &&
+                                 ((LinkName.ToLower().Contains("cv") == true) ||
+                                  (LinkName.ToLower().Contains("curriculum") == true) ||
+                                  (LinkName.ToLower().Contains("vitae") == true))
+                                 )
+                        {
+                            Ret.Add(HREF, LinkName);
+                        }
+
+                        // discard all other pdf link 
+                        else if ((HREF.Contains(".pdf") == true))
+                        { }
+
+                        else
+                        {
+                            Ret.Add(HREF, LinkName);
+                        }
+                    }
+                }
+            }
+            return Ret;
+        }
+
+
+        void web_NewWindow(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
+        #endregion
+
+        #region Read_PDF(String link)
+        private String Read_PDF(String link)
+        {
+            // link = "H:\\a.pdf";
+            PdfReader reader2 = new PdfReader((string)link);
+            String strText = String.Empty;
+            for (int page = 1; page <= reader2.NumberOfPages; page++)
+            {
+                iTextSharp.text.pdf.parser.SimpleTextExtractionStrategy its = new iTextSharp.text.pdf.parser.SimpleTextExtractionStrategy();
+                PdfReader reader = new PdfReader(link);
+                String S = iTextSharp.text.pdf.parser.PdfTextExtractor.GetTextFromPage(reader, page, its);
+                S = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(S)));
+                strText += S;
+                reader.Close();
+            }
+            return strText;
+        }
+        #endregion
+
+
+
+        private String Populate_All_Home_Pages()
+        {
+            String Line = String.Empty;                        
+            Hashtable H = new Hashtable();
+            String Pth = String.Empty;
+            ListViewItem L;
+            StreamReader SR = File.OpenText(Application.StartupPath + "\\HomePages.txt");
+            while (!SR.EndOfStream) 
+            {
+                Line = SR.ReadLine();
+                String[] Arr = Line.Split("\t".ToCharArray());
+                if ( String.IsNullOrEmpty(Arr[1]) == false) 
+                {
+                 if  (H.Contains(Arr[0]) == false)
+                 {
+                    H.Add(Arr[0], Arr[1]);
+                    L= lvw_Authors.Items.Add(Arr[0].Trim());
+                    L.SubItems.Add(Arr[1]);                   
+                 }
+                }
+            }
+            SR.Close();
+            SR.Dispose();
+            return  lvw_Authors.Items.Count.ToString() + " Authors with Homepages laoded...";
+        }
+
+        private void btn_Refresh_Click(object sender, EventArgs e)
+        {
+            What_was_Latest_Value();
+        }
+
+
+        
+    }
+}
